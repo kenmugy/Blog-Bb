48d694acf23aab4697f6142315a6828f1c7d9a17
ken <kenmugy@gmail.com>
2020-01-23 01:12:30 +0300

    implemeted Edit functionality

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const log = console.log;
   const titleInput = document.getElementById('title');
   const bodyInput = document.getElementById('body');
   const mid = document.querySelector('.mid');
   const feedback = document.getElementById('feedback');
 
-  let blogPosts = JSON.parse(localStorage.getItem('Blog Posts')) || []; // without the |this wil alwys return null|
+  const modal = document.querySelector('.myModal');
+  const close = document.getElementById('close');
+  const updateButton = document.getElementById('updateBtn');
+  const newTitle = document.getElementById('newTitle');
+  const newBody = document.getElementById('newBody');
+
+  let blogPosts = JSON.parse(localStorage.getItem('Blog Posts')) || []; // without the || this wil alwys return null
 
   document.addEventListener('submit', e => {
     e.preventDefault();
@@ -18,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       feedback.style.display = 'none';
       const blogPost = {
-        id: blogPosts.length + 1,
+        id: Math.random() * 1,
         title: titleInput.value.trim(),
         body: bodyInput.value.trim(),
         dateCreated: new Date().toLocaleString()
@@ -34,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // get from localStorage
   const getLocalStorage = () => {
     let storageString = blogPosts
-      .sort((a, b) => b.id - a.id)
+      .sort((a, b) => a.dateCreated - b.dateCreated)
       .map(
         ({ title, body, dateCreated, id }) => `
           <div class="card-panel">
@@ -57,9 +62,36 @@ document.addEventListener('DOMContentLoaded', () => {
   if (blogPosts) {
     getLocalStorage();
     const mid = document.querySelector('.mid');
+    // handle update and delete
     mid.addEventListener('click', e => {
       if (e.target.id === 'edit') {
-        // update logig goes here
+        modal.style.display = 'block';
+        let parentID = e.target.parentNode.id;
+        const bToEdit = blogPosts.find(({ id }) => id == parentID);
+        const { title, body } = bToEdit;
+        newTitle.textContent = title;
+        newBody.textContent = body;
+        updateButton.addEventListener('click', () => {
+          blogPosts = blogPosts.filter(({ id }) => id != parentID);
+          const blogPost = {
+            id: Math.random() * 1,
+            title: newTitle.textContent.trim(),
+            body: newBody.textContent.trim(),
+            dateCreated: new Date().toLocaleString()
+          };
+          blogPosts = [...blogPosts, blogPost];
+          log(blogPosts);
+          modal.style.display = 'none';
+          localStorage.setItem('Blog Posts', JSON.stringify(blogPosts));
+          getLocalStorage();
+        });
+
+        close.addEventListener('click', () => {
+          modal.style.display = 'none';
+        });
+        window.addEventListener('click', e => {
+          if (e.target.className == 'myModal') modal.style.display = 'none';
+        });
       } else if (e.target.id === 'delete') {
         let parentID = e.target.parentNode.id;
         blogPosts = blogPosts.filter(({ id }) => id != parentID);
